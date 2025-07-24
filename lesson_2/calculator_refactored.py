26ea1ddc1104ef88da3eb25b4cd2325e0cbee479
ndaege <87280269+ndaege@users.noreply.github.com>
2025-07-24 00:27:20 -0400

    added features to calculator and config file

--- lesson_2/calculator_refactored.py
+++ lesson_2/calculator_refactored.py
@@ -1,3 +1,5 @@
+import json
+
 def prompt(message):
     print(f"==> {message}")
 
@@ -9,38 +11,53 @@ def invalid_number(number_str):
 
     return False
 
-prompt("Welcome to Calculator!")
+with open('lesson_2/calc_config.json', 'r') as config_file:
+    data = json.load(config_file)
+    
+def get_param_and_lang(parameter, lang):
+    return data.get(parameter).get(lang)
 
-prompt("What's the first number?")
-number1 = input()
 
-while invalid_number(number1):
-    prompt("Hmm...that doesn't look like a valid number.")
+user_input = "y"
+while user_input != "n":
+    prompt("What language would you like to use? (en)")
+    prompt("Cuál es el idioma que prefiere usar? (es)")
+    lang = input("en/es\n")
+    
+    prompt(get_param_and_lang("welcome", lang))
+
+    prompt(get_param_and_lang("first_number", lang))
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt(get_param_and_lang("num_invalid", lang))
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm...that doesn't look like a valid number.")
+    prompt(get_param_and_lang("second_number", lang))
     number2 = input()
 
-prompt('What operation would you like to perform?\n1) Add 2) Subtract \
-    3) Multiply 4) Divide')
-operation = input()
+    while invalid_number(number2):
+        prompt(get_param_and_lang("num_invalid", lang))
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
+    prompt(get_param_and_lang("operation", lang))
     operation = input()
 
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        ouput = int(number1) - int(number2)
-    case '3':
-        ouput = int(number1) * int(number2)
-    case '4':
-        ouput = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt(get_param_and_lang("invalid_choice", lang))
+        operation = input()
 
-prompt(f"The result is {output}")
+    match operation:
+        case '1':
+            output = int(number1) + int(number2)
+        case '2':
+            output = int(number1) - int(number2)
+        case '3':
+            output = int(number1) * int(number2)
+        case '4':
+            output = int(number1) / int(number2)
+
+    prompt(f"The result is {output}")
+    print()
+    prompt(get_param_and_lang("continue", lang))
+    user_input = input().lower()
