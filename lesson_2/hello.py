a3393256898ae2477218c89da8f8610ad6fe42f6
ndaege <87280269+ndaege@users.noreply.github.com>
2025-07-23 23:24:23 -0400

    added calc and refactored calc

---
+++ lesson_2/hello.py
@@ -0,0 +1,9 @@
+"""
+Module Name
+
+A brief description of what this module does.
+
+Module contents (Functions, classes, variables, etc.) can be briefly introduced here.
+"""
+
+print("Hello world")
